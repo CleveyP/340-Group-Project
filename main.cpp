6680c63abcd46cd19fa666f8b934154d086c0c23
Azi <subalt4amail@gmail.com>
2022-04-18 13:20:18 -0700

    Initial Commit to Branch 'Azi-Sandbox'

--- main.cpp
+++ main.cpp
@@ -1,8 +1,19 @@
+/*ZRespicio branch*/
 // main file for movies group project for 340
 #include "functions.h"
 #include <iostream>
 #include <fstream>
 using namespace std;
+
+/*
+    Read CSV.
+    Get specific column from CSV.
+    Extract data from CSV and load into vector.
+    Read vector, determine duplicates.
+    - If found remove, create instance of original and incrememnt by 1.
+    Print out table rankings.
+    Library to generate charts(?)
+*/
 
 int main()
 {
