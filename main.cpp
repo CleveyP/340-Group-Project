b65d8e29967bce670df8ef7f04265cf6ce580ee3
MassiveInflatableToaster <LeSpacewhale@gmail.com>
2022-04-18 11:02:52 -0700

    Unzipped the files
    Experimental to see if they'll stay if uploaded like this

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 // main file for movies group project for 340
-#include "functions.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
-int main()
-{
-
-    return 0;
-}
+    int main()
+    {
+        ifstream inputFile;
+        inputFile.open("tmdb_5000_credits.csv");
+        if(!inputFile){
+            cout<<"It didnt open properly";
+        }else{
+            string testLine;
+            inputFile >> testLine;
+            cout<<"the first line from file is: "<<testLine;
+        }
+        return 0;
+    }
