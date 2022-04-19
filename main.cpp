7faffda7df278bdf4b1b6e87cb8ae6374670aa7f
RonnieHuangSFSU <91033357+RonnieHuangSFSU@users.noreply.github.com>
2022-04-18 18:34:27 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,20 @@
 // main file for movies group project for 340
 #include "functions.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-
+    ifstream fileIn;
+    fileIn.open("csvs//tmdb_5000_credits.csv");
+    if (!fileIn.is_open()) {
+        cout << "failed" << endl;
+    }
+    else{
+        string fileNum1;
+        fileIn >> fileNum1;
+        cout << fileNum1 << endl;
+    }
     return 0;
 }
