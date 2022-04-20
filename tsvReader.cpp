52c68d9b0a3d427b7620c8e736f8251c414b9d45
MassiveInflatableToaster <LeSpacewhale@gmail.com>
2022-04-19 23:08:25 -0700

    WIP String to Vector Conversion
    Very early stages of creating a function to convert the spaghetti vectors into more usable ones.

--- tsvReader.cpp
+++ tsvReader.cpp
@@ -19,13 +19,13 @@ using namespace std;
 // Placeholder struct for movie data
 struct movieStructTest
 {
-    string movieTitle = "";      // col 1 movie title
-    int revenue = 0;           // col 2 movie revenue
-    int budget = 0;        // col 3 movie budget
+    string movieTitle = "";     // col 1 movie title
+    int revenue = 0;            // col 2 movie revenue
+    int budget = 0;             // col 3 movie budget
     vector<string> genres = {}; // col 4 movie's genres
     string releaseDate = "";    // col 5 movie's release date TO DO: YEAR ONLY for release date
-    double rating = 0;         // col 6 movie average rating
-    int voteCount = 0;  // col 7 store number of votes towards the rating?
+    double rating = 0;          // col 6 movie average rating
+    int voteCount = 0;          // col 7 store number of votes towards the rating?
 };
 
 // Struct for storing Credits.tsv data
@@ -61,6 +61,26 @@ vector<string> delimitThis(const string &lineToSplit, char delimiter)
     return lineToWordsVector; // Return the completed vector
 }
 
+// WIP function to extract only the useful portions of a vector so we don't have all the junk data
+vector<string> convertStringToArray(string convertThis, string conversionType)
+{
+    vector<string> newArray = {};
+    // Takes in a string to convert to an array
+    // and a string that identifies which type of string this array is supposed to be
+    if (convertThis.size() > 0)
+    { // Failsafe to only run the function if the given string is not empty
+        if (conversionType == "cast")
+        {
+            cout << "cast conversion" << endl; // debug print line
+        }
+        else if (conversionType == "crew")
+        {
+            cout << "crew conversion" << endl; // debug print line
+        }
+    }
+    return newArray;
+}
+
 int main()
 {
     ifstream inputFile;
@@ -83,7 +103,7 @@ int main()
         vector<creditsStruct *> vectorOfRowVectors; // The big vector holding all of the various row structs
         int i = 0;                                  // debugging int to find out which line this might break on
 
-        while (!inputFile.eof() && i < 27) // While not at the end of the file...
+        while (!inputFile.eof()) // While not at the end of the file...
         {
             /* debugging lines
             cout << inputFile.eof() << endl;
@@ -99,9 +119,22 @@ int main()
 
             creditsStruct *newRowStruct = new creditsStruct;
             (*newRowStruct).title = currentLineVector.at(0); // Save the movie title data
-            (*newRowStruct).cast = currentLineVector.at(1);  // Save the movie cast data
-            (*newRowStruct).crew = currentLineVector.at(2);  // Save the movie crew data
-            vectorOfRowVectors.push_back(newRowStruct);      // Save this struct in the big vector of row structs
+
+            // Only store cast data if it exists and it's actually the cast data, checked if it contains "character" which is exclusive to cast
+            if (currentLineVector.size() >= 2 && currentLineVector.at(1).find("character") != std::string::npos)
+            {
+                convertStringToArray(currentLineVector.at(1), "cast");
+                (*newRowStruct).cast = currentLineVector.at(1); // Save the movie cast data
+            }
+
+            // Only store crew data if it exists and it's actually the crew data (checked by if it contains department which is exclusive to crew, != npos means it's in there somewhere)
+            if (currentLineVector.size() >= 3 && currentLineVector.at(2).find("department") != std::string::npos)
+            {
+                convertStringToArray(currentLineVector.at(2), "crew");
+                (*newRowStruct).crew = currentLineVector.at(2); // Save the movie crew data
+            }
+
+            vectorOfRowVectors.push_back(newRowStruct); // Save this struct in the big vector of row structs
 
             cout << i << endl; // Debugging line to find out how many times this runs all the way to the end of this loop
             i++;
@@ -109,14 +142,14 @@ int main()
 
         cout << endl;
         // Print elements of the structs to see if this works
-        //cout << vectorOfRowVectors.at(0)->title << endl;
-        //cout << vectorOfRowVectors.at(0)->cast << endl;
-        //cout << vectorOfRowVectors.at(0)->crew << endl;
-        // Should print out the first 28 titles before it currently crashes at Jurassic world since I haven't set it up to handle empty columns yet
-        for(int increment = 0; increment < 27; increment++){
+        // cout << vectorOfRowVectors.at(0)->title << endl;
+        // cout << vectorOfRowVectors.at(0)->cast << endl;
+        // cout << vectorOfRowVectors.at(0)->crew << endl;
+        // Testing printing loop, change increment to however many to print out
+        for (int increment = 0; increment < 5; increment++)
+        {
             cout << vectorOfRowVectors.at(increment)->title << endl;
         }
-        
     }
 
     return 0;
