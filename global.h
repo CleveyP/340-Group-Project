5f790f990e782e3023d8cd1278c45b32bd6fc302
MassiveInflatableToaster <LeSpacewhale@gmail.com>
2022-04-20 22:01:33 -0700

    Attempts at a global variable..
    Not working as intended

---
+++ global.h
@@ -0,0 +1,24 @@
+/*
+Stephen Martel
+4/20/2022
+
+Experimenting with global variables
+
+*/
+
+
+#include <vector>
+
+#ifndef global_items
+#define global_items
+
+using namespace std;
+
+extern struct combinedMovieStruct{};
+extern vector<combinedMovieStruct *> vectorOfMovieRowVectors;
+
+
+
+
+
+#endif
